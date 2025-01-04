441944a4a3b5b90363e174e8fe663909240022c6
manh0912hehe <21101121,manh@student.iuh.edu.vn>
2025-01-04 23:48:35 +0700

    thay doi qua transaction pooler

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,38 +1,38 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Option 2: Passing parameters separately (other dialects)
 const sequelize = new Sequelize(
-   process.env.DB_DATABASE_NAME,
-   process.env.DB_USERNAME,
-   process.env.DB_PASSWORD,
+   process.env.DB_DATABASE_NAME, // Database name
+   process.env.DB_USERNAME, // User (tenant)
+   process.env.DB_PASSWORD, // Password
    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "postgres",
-      logging: false,
-      dialectOptions:
-         process.env.DB_SSL === "true"
-            ? {
-                 ssl: {
+      host: process.env.DB_HOST, // Host của transaction pooler
+      port: process.env.DB_PORT, // Port của transaction pooler
+      dialect: process.env.DB_DIALECT, // PostgreSQL
+      logging: false, // Tắt log SQL
+      dialectOptions: {
+         ssl:
+            process.env.DB_SSL === "true"
+               ? {
                     require: true,
-                    rejectUnauthorized: false,
-                 },
-              }
-            : {},
-      query: {
-         raw: true,
+                    rejectUnauthorized: false, // Cho phép kết nối với chứng chỉ tự ký
+                 }
+               : {},
       },
-      timezone: "+07:00",
+      query: {
+         raw: true, // Trả về kết quả dạng object thuần
+      },
+      timezone: "+07:00", // Đặt timezone cho ứng dụng
    }
 );
 
-let connectDB = async () => {
+const connectDB = async () => {
    try {
       await sequelize.authenticate();
       console.log("Connection has been established successfully.");
    } catch (error) {
-      console.error("Unable to connect to the database:", error);
+      console.error("Unable to connect to the database:", error.message);
    }
 };
+
 module.exports = connectDB;
