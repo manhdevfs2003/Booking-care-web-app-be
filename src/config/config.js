5f3acbf9c4eddea8eb3283fcc28f3a786e058f4b
manh0912hehe <huumanhtqt2002@gmail.com>
2025-02-28 00:49:06 +0700

    change config.js db_ssl

--- src/config/config.js
+++ src/config/config.js
@@ -1,40 +1,24 @@
 require("dotenv").config();
 
 module.exports = {
-   development: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: process.env.DB_DIALECT,
-      dialectOptions: {
-         ssl:
-            process.env.DB_SSL === "true"
-               ? {
-                    require: true,
-                    rejectUnauthorized: false,
-                 }
-               : {},
-      },
-      timezone: "+07:00", // Đặt timezone phù hợp
-   },
-   production: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: process.env.DB_DIALECT,
-      dialectOptions: {
-         ssl:
-            process.env.DB_SSL === "true"
-               ? {
-                    require: true,
-                    rejectUnauthorized: false,
-                 }
-               : {},
-      },
-      timezone: "+07:00",
-   },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: process.env.DB_SSL === "true" ? { ssl: { require: true, rejectUnauthorized: false } } : {},
+    timezone: "+07:00",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: process.env.DB_SSL === "true" ? { ssl: { require: true, rejectUnauthorized: false } } : {},
+    timezone: "+07:00",
+  },
 };
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -1,24 +1,28 @@
-'use strict';
+"use strict";
 
 module.exports = {
-
-
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [{
-      email: 'admin@gmail.com',
-      password: '123456',
-      firstName: 'Nguyen',
-      lastName: 'Rin',
-      address: 'HCM',
-      gender: 1,
-      typeRole: 'ROLE',
-      keyRole: 'R1',
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "admin@gmail.com",
+          password: "123456",
+          firstName: "Nguyen",
+          lastName: "Rin",
+          address: "HCM",
+          gender: 1,
+          typeRole: "ROLE",
+          keyRole: "R1",
 
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
-//viet code in ra so 1  
+  //viet code in ra so 1
   down: async (queryInterface, Sequelize) => {
     /**
      * Add commands to revert seed here.
@@ -26,5 +30,5 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
